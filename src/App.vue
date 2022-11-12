8417ea383378c9b9cabe995838a194fb410aee10
Bonus TM <bonus.tm@gmail.com>
2022-11-12 13:36:23 +0300

    Prevent placing celestials too close to each other

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,30 @@
 <script setup>
-import {onMounted, reactive, ref} from 'vue'
-import {wanderer} from '@/classes/Wanderer.js'
-import {Celestial} from '@/classes/Celestial.js'
-import {setOpacity} from '@/util/colors.js'
-import {rand, round} from '@/util/numbers.js'
-import {params} from '@/params.js'
-import {canvas, context, backgroundColor} from '@/store'
-import {clearCanvas, lineGradient} from '@/services/draw.js'
+import {onMounted, ref} from 'vue'
+import {wanderer} from '@/classes/Wanderer'
+import {setOpacity} from '@/util/colors'
+import {rand, round} from '@/util/numbers'
+import {params} from '@/params'
+import {canvas, context, backgroundColor, colorCelestial, colorWanderer} from '@/store'
+import {circle, clearCanvas, lineGradient} from '@/services/draw'
 import Settings from '@/components/Settings.vue'
 import Logo from '@/components/Logo.vue'
-import {calcForces, drawForces} from '@/classes/forces.js'
+import {calcForces, drawForces} from '@/classes/forces'
 import SvgCog from '@/components/SvgCog.vue'
+import {distance} from '@/services/vector'
 
 let raf
-
-let colorBg
-let colorCelestial
-let colorWanderer
 
 let celestials = []
 
 let time
 
-let velocityMin = ref(Infinity)
-let velocityMax = ref(0)
-
 onMounted(() => {
   let style = getComputedStyle(document.body)
-  colorBg = style.getPropertyValue('--color-bg').trim()
-  colorCelestial = style.getPropertyValue('--color-celestial').trim()
-  colorWanderer = style.getPropertyValue('--color-wanderer').trim()
 
   backgroundColor.value = style.getPropertyValue('--color-bg').trim()
 
-  console.log('mounted', {colorBg, colorCelestial, colorWanderer})
+  colorCelestial.value = style.getPropertyValue('--color-celestial').trim()
+  colorWanderer.value = style.getPropertyValue('--color-wanderer').trim()
 
   let minDimension = Math.min(canvas.value.width, canvas.value.height)
   params.radiusMin = Math.round(minDimension / 100)
@@ -54,23 +45,37 @@ const createCelestials = () => {
   let {width, height} = canvas.value
   let padding = 0.1
   for (let i = 0; i < params.celestialsCount; i++) {
-    let body = Object.assign(Object.create(Celestial), {
-      // x: 500,
-      // y: 250,
-      // mass: params.massMax,
+    let mass = rand(params.massMin, params.massMax)
+    let radius = Math.sqrt((params.radiusMax ** 2) * mass / params.massMax)
+    let x
+    let y
 
-      x: rand(width * padding, width * (1 - padding)),
-      y: rand(height * padding, height * (1 - padding)),
-      mass: rand(params.massMin, params.massMax),
-      color: colorCelestial,
-    })
-    celestials.push(body)
+    let tooClose = true
+    let attempt = 0
+
+    while (tooClose && attempt < params.celestialsCount * 100) {
+      tooClose = false
+      attempt++
+
+      x = rand(width * padding, width * (1 - padding))
+      y = rand(height * padding, height * (1 - padding))
+
+      for (let body of celestials) {
+        let dist = distance([x, y], [body.x, body.y])
+        if (dist < radius + body.radius + Math.max(radius, body.radius)) {
+          tooClose = true
+          break
+        }
+      }
+    }
+
+    celestials.push({x, y, mass, radius})
   }
 }
 
 const drawCelestials = () => {
   for (let body of celestials) {
-    body.draw()
+    circle(body.x, body.y, body.radius, colorCelestial.value)
   }
 }
 
@@ -143,9 +148,9 @@ const drawLine = (x, y) => {
   clearCanvas()
   drawCelestials()
 
-  lineGradient(s.x, s.y, x, y, 1,
-    setOpacity(colorWanderer, wanderer.maxTrailOpacity),
-    setOpacity(colorWanderer, wanderer.minTrailOpacity)
+  lineGradient(s.x, s.y, x, y, wanderer.radius,
+    setOpacity(colorWanderer.value, wanderer.maxTrailOpacity),
+    setOpacity(colorWanderer.value, wanderer.minTrailOpacity)
   )
 }
 
